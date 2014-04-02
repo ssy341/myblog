305f0ceab937c94676179645fca004b84a3188ca
shansiyi <keithssy@gmail.com>
2014-04-02 15:11:09 +0800

    add includes

--- js/gbtags_datatables.js
+++ js/gbtags_datatables.js
@@ -1,3 +1,6 @@
+---
+layout: js
+---
 var oTable;
 $(document).ready(function(){
 	
@@ -24,13 +27,13 @@ function initTable(id){
 	
 	var table =  $("#"+id).dataTable({
 	     	"iDisplayLength":5,
-	        "sAjaxSource": "http://ssy341.github.io/myblog/src/array.txt",
+	        "sAjaxSource": "{{ site.baseurl }}src/array.txt",
 	        'bPaginate': true,  
 	        "bProcessing": true,                   
 	        "sDom": "<'row-fluid'<'span6 myBtnBox'><'span6'f>r>t<'row-fluid'<'span6'i><'span6 'p>>",
 			"sPaginationType": "bootstrap",
 			"oLanguage": {
-				"sUrl": "http://ssy341.github.io/myblog/js/zh-CN.txt"
+				"sUrl": "{{ site.baseurl }}js/zh-CN.txt"
 			},
 			"fnInitComplete": function(oSettings, json){
 				console.log("init complete");
